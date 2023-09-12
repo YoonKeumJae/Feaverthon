47ca43ff7856557f225bc3617b0de7c06fe33784
baekggum <c3man4@naver.com>
2023-09-13 00:41:53 +0900

    feat #1 : map

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 export default function BasicMap() {
   return (
--- src/pages/map/map.jsx
+++
@@ -1,8 +0,0 @@
-import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
-
-export default function useKakaoLoader() {
-  useKakaoLoaderOrigin({
-    appkey: ,
-    libraries: ["clusterer", "drawing", "services"],
-  });
-}
