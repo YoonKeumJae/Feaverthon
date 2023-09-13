0c309351d493773ca9a391d29a45554f47cb5415
baekggum <c3man4@naver.com>
2023-09-14 00:12:21 +0900

    feat #1 : making..2

--- src/pages/map/components/Mapbottom/index.jsx
+++ src/pages/map/components/Mapbottom/index.jsx
@@ -2,14 +2,32 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Mapbottom() {
-  const Bottom = styled.div`
+  const Bottom = styled.ul`
+    display: flex;
+    padding: 0;
+    margin: 0;
+    width: 100%;
     height: 10vh;
     border: 1px solid black;
   `;
-
+  
+  const Bottomli = styled.li`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    width: 20%;
+    list-style-type: none;
+  `;
   return (
     <div>
-      <Bottom />
+      <Bottom >
+        <Bottomli><img src="#" alt="#" /></Bottomli>
+        <Bottomli><img src="#" alt="#" /></Bottomli>
+        <Bottomli><img src="#" alt="#" /></Bottomli>
+        <Bottomli><img src="#" alt="#" /></Bottomli>
+        <Bottomli><img src="#" alt="#" /></Bottomli>
+      </Bottom>
     </div>
   );
 }
