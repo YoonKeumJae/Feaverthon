cde92f0c208af377f1bb316354f5ea8063203aaa
KimSound424 <ykj1512@gmail.com>
2023-09-13 18:15:51 +0900

    [Feature] 비밀번호 일치 확인 기능 추가

--- src/pages/auth/signUp/SignUP.jsx
+++ src/pages/auth/signUp/SignUP.jsx
@@ -3,6 +3,22 @@ import { React, useState } from "react";
 function SignUp() {
   const [email, setEmail] = useState("");
   const [isValidEmail, setIsValidEmail] = useState(true);
+  const [password, setPassword] = useState("");
+  const [isSamePassword, setIsSamePassword] = useState(true);
+
+  // 비밀번호 입력 칸 변화를 감지함
+  function inputPassword(e) {
+    setPassword(e.target.value);
+  }
+
+  // 비밀번호 확인 칸 변화를 감지함
+  function checkPassword(e) {
+    if (password === e.target.value) {
+      setIsSamePassword(true);
+    } else {
+      setIsSamePassword(false);
+    }
+  }
 
   // 이메일 유효성 검사
   function validateEmail(emailToCheck) {
@@ -25,8 +41,17 @@ function SignUp() {
         <span>Email</span>
         <input type="email" placeholder="Email" onChange={handleEmailChange} />
         {!isValidEmail && <span>이메일 형식이 올바르지 않습니다.</span>}
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Password Check" />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={inputPassword}
+        />
+        <input
+          type="password"
+          placeholder="Password Check"
+          onChange={checkPassword}
+        />
+        {!isSamePassword && <span>비밀번호가 일치하지 않습니다.</span>}
         <button type="submit">Sign In</button>
       </form>
     </div>
