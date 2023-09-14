8f0ffebb2bb9f18a76d7ca72e600faecdc3f6895
도경 <lee7198@naver.com>
2023-09-14 10:19:39 +0900

    feat #2 add bottomNav

--- src/pages/favorite/index.jsx
+++ src/pages/favorite/index.jsx
@@ -1,14 +1,19 @@
 import { Container } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { List } from "@phosphor-icons/react";
 import Header from "./conponents/Header";
 import BottomNav from "../../components/BottomNav";
 
 export default function Favorites() {
+  const [centerMenu, setCenterMenu] = useState({
+    path: "/",
+    icon: <List />,
+  });
   return (
     <Container maxWidth="xs" sx={{ p: 0 }}>
       <Header />
       즐겨찾기 페이지
-      <BottomNav />
+      <BottomNav centerMenu={centerMenu} setCenterMenu={setCenterMenu} />
     </Container>
   );
 }
