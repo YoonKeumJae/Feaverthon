515aa9a39f8216754896daf108c18a99b5aae554
도경 <lee7198@naver.com>
2023-09-14 23:17:52 +0900

    faet dynamic category

--- src/components/BottomNav/index.jsx
+++ src/components/BottomNav/index.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import { IconButton } from "@mui/material";
 import PropTypes from "prop-types";
-import axios from "axios";
 import { StyledFooter, StyledCenterButton } from "./style";
 import menu from "./data";
 
@@ -11,15 +10,6 @@ function BottomNav({ centerMenu }) {
   const pafilldex = menu.findIndex(
     (object) => location.pathname && object.path === location.pathname,
   );
-
-  useEffect(() => {
-    axios
-      .get("http://121.55.117.123:8005", {
-        withCredentials: true, // 클라이언트와 서버가 통신할때 쿠키와 같은 인증 정보 값을 공유하겠다는 설정
-      })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  });
 
   return (
     <StyledFooter>
--- src/pages/favorite/index.jsx
+++ src/pages/favorite/index.jsx
@@ -5,7 +5,7 @@ import Header from "./conponents/Header";
 import BottomNav from "../../components/BottomNav";
 import ReviewItem from "./conponents/ReviewItem";
 
-const tab = ["전체", "대구", "경북", "서울", "부산"];
+// const tab = ["전체", "대구", "경북", "서울", "부산"];
 const restaurant = [
   {
     title: "뉴살라딘",
@@ -14,6 +14,7 @@ const restaurant = [
     tel: "053-123-1234",
     rating: 4.9,
     reviews: 10,
+    location: "대구",
   },
   {
     title: "뉴살라딘2",
@@ -22,6 +23,7 @@ const restaurant = [
     tel: "053-123-1234",
     rating: 4.9,
     reviews: 1,
+    location: "대구",
   },
   {
     title: "뉴살라딘3",
@@ -30,10 +32,19 @@ const restaurant = [
     tel: "053-123-1234",
     rating: 4.9,
     reviews: 2,
+    location: "부산",
   },
 ];
 
 export default function Favorites() {
+  // 현재 이름순으로 정렬되어 있는데 추후 데이터량 기준으로 정렬 필요할듯
+  const [tab] = useState(
+    Object.keys(
+      Object.fromEntries(
+        restaurant.map((item) => item.location).map((v) => [v, null]),
+      ),
+    ),
+  );
   const [tabIndex, setTabIndex] = useState(0);
   const [centerMenu, setCenterMenu] = useState({
     path: "/",
@@ -43,13 +54,14 @@ export default function Favorites() {
     <Container maxWidth="xs" sx={{ p: 0, pt: 7 }}>
       <Header />
 
+      {/* 지역 카테고리 */}
       <Tabs
         value={tabIndex}
-        onChange={(_, newValue) => {
+        onChange={(e, newValue) => {
           setTabIndex(newValue);
         }}
       >
-        {tab.map((item) => (
+        {["전체"].concat(tab).map((item) => (
           <Tab label={item} key={item} />
         ))}
       </Tabs>
@@ -58,17 +70,35 @@ export default function Favorites() {
         전체 즐겨찾기 {restaurant.length}개{" "}
       </Typography>
 
-      {restaurant.map((item) => (
-        <ReviewItem
-          title={item.title}
-          category={item.category}
-          address={item.address}
-          tel={item.tel}
-          rating={item.rating}
-          reviews={item.reviews}
-        />
-      ))}
-
+      {/* 식당 리스트 */}
+      {tabIndex === 0
+        ? // 전체 지역
+          restaurant.map((item) => {
+            return (
+              <ReviewItem
+                key={item.title}
+                title={item.title}
+                category={item.category}
+                address={item.address}
+                tel={item.tel}
+                rating={item.rating}
+                reviews={item.reviews}
+              />
+            );
+          })
+        : restaurant
+            .filter(({ location }) => location === tab[tabIndex])
+            .map((item) => (
+              <ReviewItem
+                key={item.title}
+                title={item.title}
+                category={item.category}
+                address={item.address}
+                tel={item.tel}
+                rating={item.rating}
+                reviews={item.reviews}
+              />
+            ))}
       <BottomNav centerMenu={centerMenu} setCenterMenu={setCenterMenu} />
     </Container>
   );
