2a682c2161ec504d78c9a89065b64daa9cbdaa54
KimSound424 <ykj1512@gmail.com>
2023-09-15 22:03:24 +0900

    merge

--- src/pages/auth/signIn/SignIn.jsx
+++ src/pages/auth/signIn/SignIn.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import StyledSignIn from "./styles/auth/signIn/StyledSignIn";
+import StyledSignIn from "../../../styles/auth/StyledSignIn";
 
 function SignIn() {
   return (
@@ -8,7 +8,9 @@ function SignIn() {
       <form>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
+        <span>Forgot Password?</span>
         <button type="submit">Sign In</button>
+        <button type="button">Sign In with Naver</button>
       </form>
       <span>{" Don't "} have an account? Sign Up</span>
     </StyledSignIn>
--- src/styles/auth/StyledSignIn.jsx
+++ src/styles/auth/StyledSignIn.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
-const StyledSignIn = styled.div``;
+const StyledSignIn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default StyledSignIn;
